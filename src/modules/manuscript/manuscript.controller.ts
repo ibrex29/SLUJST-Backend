055346f3ae636bb733cfa8322fa3b64f1a61509c
ibrahim muhammad <93261900+ibrex29@users.noreply.github.com>
2025-08-03 23:56:46 +0100

    Update manuscript.controller.ts

--- src/modules/manuscript/manuscript.controller.ts
+++ src/modules/manuscript/manuscript.controller.ts
@@ -17,7 +17,7 @@ import { RejectManuscriptDto } from './dto/update-manuscript.dto';
 
 @ApiTags('manuscripts')
 @ApiBearerAuth()
-@Controller({ path: 'authmanuscriptsor', version: '1' })
+@Controller({ path: 'manuscripts', version: '1' })
 @UseGuards(RolesGuard)
 export class ManuscriptController {
   constructor(private readonly manuscriptService: ManuscriptService) {}
