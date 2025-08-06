d6ab6a294f738af80d1413cfe8648cad70ce95e9
ibrahim muhammad <93261900+ibrex29@users.noreply.github.com>
2025-08-06 12:30:27 +0100

    Update upload.controller.ts

--- src/modules/upload/upload.controller.ts
+++ src/modules/upload/upload.controller.ts
@@ -20,7 +20,7 @@ import { Request } from 'express';
 import { Public } from 'src/common/constants/routes.constant';
 @Public()
 @ApiTags('Upload')
-@Controller('upload')
+@Controller({ path: 'upload', version: '1' })
 export class UploadController {
   @Post()
   @ApiOperation({ summary: 'Upload a file to the server (PDF/Images only)' })
