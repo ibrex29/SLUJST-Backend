307a106ae066ac2ce1d3890b3983e7081d0b53c2
ibrahim muhammad <93261900+ibrex29@users.noreply.github.com>
2025-08-06 12:58:54 +0100

    Update upload.controller.ts

--- src/modules/upload/upload.controller.ts
+++ src/modules/upload/upload.controller.ts
@@ -41,7 +41,7 @@ export class UploadController {
   @UseInterceptors(
     FileInterceptor('file', {
       storage: diskStorage({
-        destination: './assets',
+        destination: '/var/www/slujst/assets',
         filename: (req, file, cb) => {
           const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
           cb(null, `${file.fieldname}-${uniqueSuffix}${extname(file.originalname)}`);
