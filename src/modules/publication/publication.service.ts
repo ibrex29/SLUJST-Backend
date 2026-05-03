9b0ccff43cabb7f5d0f773df4f7875ffd1563cf7
ibrahim muhammad <ibrex29@gmail.com>
2026-05-03 23:14:33 +0100

    paginated res

--- src/modules/publication/publication.service.ts
+++ src/modules/publication/publication.service.ts
@@ -280,55 +280,6 @@ export class PublicationService {
     return { data: publicationsWithReactions };
   }
 
-  // async featurePublication(
-  //   pubId: string,
-  //   userId: string,
-  //   priority = 1,
-  //   expiresAt?: Date,
-  // ) {
-  //   const publication = await this.prisma.publication.findUnique({
-  //     where: { id: pubId },
-  //     select: { id: true, isPublished: true, isActive: true },
-  //   });
-
-  //   if (!publication) {
-  //     throw new NotFoundException('Publication not found.');
-  //   }
-
-  //   if (!publication.isActive) {
-  //     throw new BadRequestException(
-  //       'Only published publications can be featured.',
-  //     );
-  //   }
-
-  //   return this.prisma.featuredPublication.upsert({
-  //     where: { publicationId: pubId },
-  //     update: {
-  //       featuredById: userId,
-  //       featuredAt: new Date(),
-  //       priority,
-  //       expiresAt,
-  //     },
-  //     create: {
-  //       publicationId: pubId,
-  //       featuredById: userId,
-  //       priority,
-  //       expiresAt,
-  //     },
-  //     include: { publication: true },
-  //   });
-  // }
-
-  // async unfeaturePublication(pubId: string) {
-  //   try {
-  //     return await this.prisma.featuredPublication.delete({
-  //       where: { publicationId: pubId },
-  //     });
-  //   } catch {
-  //     throw new NotFoundException('This publication is not featured.');
-  //   }
-  // }
-
   async getLatestIssues(take: number = 8) {
     return await this.prisma.issue.findMany({
       take,
