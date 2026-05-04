5ed310b52ff9de64567bab431b5cd3e0f79e9500
ibrahim muhammad <ibrex29@gmail.com>
2026-05-04 01:16:24 +0100

    packages upgrade

--- prisma/prisma.service.ts
+++ prisma/prisma.service.ts
@@ -1,9 +1,24 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { Prisma, PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 import { FetchDTO, PaginationResultDTO } from 'src/common/dto';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const pool = new Pool({
+      connectionString: process.env.DATABASE_URL,
+    });
+
+    const adapter = new PrismaPg(pool);
+
+    super({ adapter });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
@@ -12,41 +27,44 @@ export class PrismaService extends PrismaClient implements OnModuleInit, OnModul
     await this.$disconnect();
   }
 
-async paginate<T>(
-  modelName: Prisma.ModelName,
-  {
-    query,
-    where,
-    include = {},
-    orderBy = undefined,
-  }: {
-    query: FetchDTO & { sortField?: string };
-    where: object;
-    include?: object;
-    orderBy?: object;
-  },
-): Promise<PaginationResultDTO<T>> {
-  const { skip, limit } = query;
+  async paginate<T>(
+    modelName: Prisma.ModelName,
+    {
+      query,
+      where,
+      include = {},
+      orderBy = undefined,
+    }: {
+      query: FetchDTO & { sortField?: string };
+      where: object;
+      include?: object;
+      orderBy?: object;
+    },
+  ): Promise<PaginationResultDTO<T>> {
+    const { skip, limit } = query;
 
-  const findOption: {
-    skip: number;
-    take: number;
-    where: object;
-    include: object;
-    orderBy?: object;
-  } = {
-    skip,
-    take: limit,
-    where,
-    include,
-  };
-  if (orderBy) findOption.orderBy = orderBy;
+    const findOption: {
+      skip: number;
+      take: number;
+      where: object;
+      include: object;
+      orderBy?: object;
+    } = {
+      skip,
+      take: limit,
+      where,
+      include,
+    };
 
-  const [count, rows] = await Promise.all([
-    this[modelName].count({ where }),
-    this[modelName].findMany(findOption),
-  ]);
+    if (orderBy) findOption.orderBy = orderBy;
 
-  return new PaginationResultDTO(rows, count, query);
-}
+    const model = this[modelName] as any;
+
+    const [count, rows] = await Promise.all([
+      model.count({ where }),
+      model.findMany(findOption),
+    ]);
+
+    return new PaginationResultDTO(rows, count, query);
+  }
 }
